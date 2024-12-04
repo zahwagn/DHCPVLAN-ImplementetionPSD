7ef9e1fa7c9d3cf2588a254956b8077e02913050
BonifasiusRaditya <radityahendrianto@gmail.com>
2024-12-05 03:02:11 +0700

    Server DHCP dah bisa

--- fsm_dhcp.vhdl
+++ fsm_dhcp.vhdl
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use std.textio.all;
 
 entity DHCP_Server is
     Port ( clk         : in  STD_LOGIC;
@@ -12,7 +12,7 @@ entity DHCP_Server is
            mulai       : in  STD_LOGIC;                       -- Start Process
            ip_address  : out STD_LOGIC_VECTOR(31 downto 0);   -- Allocated IP Address
            gateway     : out STD_LOGIC_VECTOR(31 downto 0);   -- Default Gateway
-           subnet_out  : out STD_LOGIC_VECTOR(31 downto 0);    -- Subnet Mask
+           subnet_out  : out STD_LOGIC_VECTOR(31 downto 0);   -- Subnet Mask
            selesai     : out STD_LOGIC                         -- Indication of Process Completion
            );
 end DHCP_Server;
@@ -21,13 +21,35 @@ architecture Behavioral of DHCP_Server is
 
     type state_type is (idle, discover, offer, request, ack, finish);
     signal current_state, next_state : state_type;
-    
-    signal ip_pool        : STD_LOGIC_VECTOR(31 downto 0);  -- To keep track of the IP pool
-    signal allocated_ip   : STD_LOGIC_VECTOR(31 downto 0);  -- Currently allocated IP
-    signal count          : integer := 0;                    -- Counter for the loop
+
+    signal last_ip        : std_logic_vector(31 downto 0) := (others => '0'); -- Tracking last IP assigned
+    signal allocated_ip   : STD_LOGIC_VECTOR(31 downto 0) := (others => '0'); 
+
+    file input_file  : text open read_mode is "dhcp_input.txt";
+    file output_file : text open write_mode is "dhcp_output.txt";
+
+    -- Procedure to write to output.txt
+    procedure write_ip_to_file(
+        variable ip_to_write : in std_logic_vector(31 downto 0)
+    ) is
+        variable write_buffer : line;
+        variable ip_str        : string(1 to 32);  -- Buffer to store IP as string
+    begin
+        -- Convert std_logic_vector to string
+        for i in 1 to 32 loop
+            if ip_to_write(32 - i) = '1' then
+                ip_str(i) := '1';
+            else
+                ip_str(i) := '0';
+            end if;
+        end loop;
+
+        -- Write to file
+        write(write_buffer, ip_str);
+        writeline(output_file, write_buffer);
+    end procedure;
 
 begin
-
     -- FSM Process
     process(clk, reset)
     begin
@@ -39,60 +61,63 @@ begin
     end process;
 
     -- State Machine Logic
-    process(current_state, num_devices)
+    process(current_state, mulai, num_devices)
+        variable host_bits : integer := 0; -- Count of '0' in subnet mask
+        variable set_gateway   : STD_LOGIC_VECTOR(7 downto 0);
+        variable set_ip        : STD_LOGIC_VECTOR(7 downto 0);
+        variable count        : integer := 0;
+        variable temp_ip      : std_logic_vector(31 downto 0);  -- Temporary variable for allocated IP
+        variable var_last_ip  : std_logic_vector(31 downto 0) := (others => '0'); -- Tracking last IP assigned
     begin
         -- Default output values
         next_state <= current_state;
-        ip_address <= (others => '0');
-        gateway <= (others => '0');
-        subnet_out <= (others => '0');
+
         case current_state is
             when idle =>
+                ip_address <= (others => '0');
+                gateway <= (others => '0');
+                subnet_out <= (others => '0');
                 if mulai = '1' and count < num_devices then
                     next_state <= discover;
+                    count := count + 1;
+                    selesai <= '0';
                 else
                     next_state <= idle;
+                    selesai <= '1';
                 end if;
 
             when discover =>
-                selesai <= '0';  -- Indicate that the process is ongoing
-                -- Discover state: the server receives a DHCPDISCOVER message
-                -- The server prepares to offer an IP
+                -- Set gateway and subnet mask
+                set_gateway := std_logic_vector(unsigned(subnet_mask(7 downto 0)) + 1);
+                gateway <= network(31 downto 8) & set_gateway;
+                if count = 1 then last_ip <= network(31 downto 8) & set_gateway;
+                end if;
+
                 next_state <= offer;
 
             when offer =>
-                -- Offer state: the server offers an IP address
-                -- The IP address to offer is network + 1 (the first available IP)
-                ip_address <= network + "00000001";  -- Assign first IP
-                gateway <= network + "00000001";    -- Assign default gateway
-                subnet_out <= subnet_mask;          -- Assign subnet mask
+                -- Calculate next IP
+                set_ip := std_logic_vector(unsigned(last_ip(7 downto 0)) + 1);
+                allocated_ip <= network(31 downto 8) & set_ip;
+
+                subnet_out <= subnet_mask;
                 next_state <= request;
 
             when request =>
-                -- Request state: the server receives DHCPREQUEST
+                ip_address <= allocated_ip;
+                last_ip <= allocated_ip;
                 next_state <= ack;
 
             when ack =>
-                -- Acknowledge state: The server sends DHCPACK
-                -- Finalize the allocation of the IP address
                 next_state <= finish;
 
             when finish =>
-                -- Finish state: DHCP process is complete
-                selesai <= '1';  -- Indicate that the process is complete
+                -- Write the allocated IP to the output file
+                temp_ip := allocated_ip;  -- Use temporary variable
+                write_ip_to_file(temp_ip);  -- Call procedure to write to file
                 next_state <= idle;
-        end case;
-    end process;
 
-    -- Looping Construct (Simple Example for IP Address Pool Allocation)
-    process(num_devices)
-    begin
-        -- Initialize the IP pool based on the network address
-        ip_pool <= network + 1;  -- Starting IP in the network range
-        for i in 1 to num_devices loop
-            -- Increment the IP pool for each device
-            ip_pool <= ip_pool + 1;  -- This is the signal assignment
-        end loop;
+        end case;
     end process;
 
 end Behavioral;
